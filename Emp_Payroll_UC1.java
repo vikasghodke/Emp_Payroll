8a65494df591c33fede4cc69e42dd0ad2ac9b265
vikas ghodke <vikasghodke468@gmail.com>
2023-12-22 15:03:14 +0530

    check employee present or not

--- Emp_Payroll_UC1.java
+++ Emp_Payroll_UC1.java
@@ -1,4 +1,20 @@
 
 public class Emp_Payroll_UC1 {
+	public static void main(String args[]){
+		int is_Full_Time=1;
+		double empCheck = Math.floor(Math.random() *10)%2;
 
+		if(empCheck == is_Full_Time)
+		{
+				System.out.println("Employee is present");
+		}
+		else
+		{
+				System.out.println("Employee is Absent");
+		}
 }
+}
+
+
+
+
